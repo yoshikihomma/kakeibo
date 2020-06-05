bb0d3685a5864de2fc23b962b47f09bfe7133a45
本間睦貴 <yoshikihomma@gmail.com>
2020-06-05 13:21:48 +0900

    未入力項目のチェック機能追加

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,170 +1,142 @@
-'use strict';
+"use strict";
 
-$(function () {
-  var $totalNum = $('.p-total__num');
-  var $foodsNum = $('.p-foods__num');
-  var $commodityNum = $('.p-commodity__num');
-  var $publicNum = $('.p-public__num');
-  var $modalOpen = $('.js-modalOpen');
-  var $modalClose = $('.js-modalClose');
-  var $inputArea = $('.p-inputArea');
-  var $inputAreaInner = $('.p-inputArea__inner');
-  var $addItem = $('.js-addItem');
-  var $itemDate = $('.p-itemDate');
-  var $itemName = $('.p-itemName');
-  var $itemType = $('.p-itemType');
-  var $itemNum = $('.p-itemNum');
-  var $attention = $('<p class="p-attention">未入力の項目があります。</p>');
+var $PRICE = document.getElementsByClassName('p-price');
+var $INPUT_TYPE = document.getElementsByClassName('p-itemtype');
+var $INPUT_PRICE = document.getElementsByClassName('p-itemprice');
+var app = new Vue({
+  el: "#app",
+  data: {
+    prices: {
+      total: 0,
+      foods: 0,
+      commodity: 0,
+      "public": 0
+    },
+    showModal: true,
+    showAttention: false,
+    keyArray: ['date', 'name', 'type', 'price'],
+    defaultValues: {
+      'date': '',
+      'name': '',
+      'type': '',
+      'price': 0
+    },
+    tempObj: {
+      'date': '',
+      'name': '',
+      'type': '選択してください',
+      'price': '0'
+    },
+    lists: [],
+    listCount: 0
+  },
+  methods: {
+    addList: function addList() {
+      var tempUpdate = function tempUpdate() {
+        for (var i = 0; i < app.keyArray.length; i++) {
+          app.tempObj[app.keyArray[i]] = document.getElementsByClassName("p-item".concat(app.keyArray[i]))[0].value;
+        }
 
-  var latestNum = function latestNum() {
-    return localStorage.getItem('latestNum');
-  };
+        console.log(app.tempObj);
+      };
 
-  var listNum = latestNum();
-  var itemLengthArray = [];
-  var tableLength = itemLengthArray.length;
-  var tempObj = {
-    '日時': '',
-    '商品名': '',
-    '分類': '',
-    '金額': 0
-  };
-  var numObj = {
-    '合計': 0,
-    '食品': 0,
-    '日用品': 0,
-    '公共料金': 0
-  }; // ブラウザ更新時、DBからリストを取得
+      var setStrage = function setStrage() {
+        app.listCount++;
+        localStorage.setItem(app.listCount, JSON.stringify(app.tempObj));
+        localStorage.setItem('latestCount', app.listCount);
+        console.log(app.lists);
+      };
 
-  var table = function table() {
-    for (var i = 1; i <= latestNum(); i++) {
-      itemLengthArray.push(JSON.parse(localStorage.getItem(i)));
+      var setLists = function setLists() {
+        app.lists.push(app.tempObj);
+      };
+
+      var typeCulculater = function typeCulculater() {
+        var type = app.lists[app.listCount - 1].type;
+        app.prices[type] += parseInt(app.lists[app.listCount - 1].price);
+      };
+
+      var totalCulculater = function totalCulculater() {
+        console.log(111);
+        app.prices['total'] += parseInt(app.lists[app.listCount - 1].price);
+      };
+
+      var resetInput = function resetInput() {
+        document.itemInput.reset();
+      };
+
+      var showAttention = function showAttention() {
+        app.showAttention = true;
+      };
+
+      var hiddenAttention = function hiddenAttention() {
+        app.showAttention = false;
+      };
+
+      if ($INPUT_TYPE[0].value === '選択してください' || $INPUT_PRICE[0].value === "") {
+        showAttention();
+      } else {
+        tempUpdate();
+        setStrage();
+        setLists();
+        typeCulculater();
+        totalCulculater();
+        resetInput();
+        hiddenAttention();
+      }
     }
-  };
-
-  table(); // ブラウザ更新時、各分野の合計を計算して表示する
-
-  var culculation = function culculation() {
-    tableLength = itemLengthArray.length;
-
-    var totalCulculation = function totalCulculation() {
-      for (var i = 0; i <= tableLength - 1; i++) {
-        numObj.合計 += parseInt(itemLengthArray[i].金額);
-      }
-
-      $totalNum.text(numObj.合計);
-    };
-
-    var foodsCulculation = function foodsCulculation() {
-      for (var i = 0; i <= tableLength - 1; i++) {
-        if (itemLengthArray[i].分類 === '食品') {
-          numObj.食品 += parseInt(itemLengthArray[i].金額);
-        }
-      }
-
-      $foodsNum.text(numObj.食品);
-    };
-
-    var commodityCulculation = function commodityCulculation() {
-      for (var i = 0; i <= tableLength - 1; i++) {
-        if (itemLengthArray[i].分類 === '日用品') {
-          numObj.日用品 += parseInt(itemLengthArray[i].金額);
-        }
-      }
-
-      $commodityNum.text(numObj.日用品);
-    };
-
-    var publicCulculation = function publicCulculation() {
-      for (var i = 0; i <= tableLength - 1; i++) {
-        if (itemLengthArray[i].分類 === '公共料金') {
-          numObj.公共料金 += parseInt(itemLengthArray[i].金額);
-        }
-      }
-
-      $publicNum.text(numObj.公共料金);
-    };
-
-    totalCulculation();
-    foodsCulculation();
-    commodityCulculation();
-    publicCulculation();
-  };
-
-  culculation(); // 入力画面モーダルの開閉
-
-  var eventModal = function eventModal() {
-    $modalOpen.on('click', function () {
-      $inputArea.removeClass('is-hidden');
-    });
-    $modalClose.on('click', function () {
-      $inputArea.addClass('is-hidden');
-      resetInput();
-    });
-  };
-
-  eventModal(); // 非同期でのリスト追加、更新
-
-  var eventAddItem = function eventAddItem() {
-    $addItem.on('click', function () {
-      funcAddItem();
-    });
-  };
-
-  eventAddItem(); // eventAddItem()での処理
-
-  var showAttention = function showAttention() {
-    $inputAreaInner.prepend($attention);
-  };
-
-  var hiddenAttention = function hiddenAttention() {
-    $('.p-attention').remove();
-  };
-
-  var setTempObj = function setTempObj() {
-    tempObj.日時 = $itemDate.val();
-    tempObj.商品名 = $itemName.val();
-    tempObj.分類 = $itemType.val();
-    tempObj.金額 = $itemNum.val();
-  };
-
-  var addListNum = function addListNum() {
-    listNum++;
-  };
-
-  var addLocalStorage = function addLocalStorage() {
-    localStorage.setItem(listNum, JSON.stringify(tempObj));
-    localStorage.setItem('latestNum', listNum);
-  };
-
-  var addTable = function addTable() {
-    itemLengthArray.push(JSON.parse(localStorage.getItem(latestNum())));
-  };
-
-  var update = function update() {
-    var addedItemType = itemLengthArray[latestNum() - 1].分類;
-    numObj[addedItemType] += parseInt(itemLengthArray[latestNum() - 1].金額);
-    $("[data-type=\"".concat(addedItemType, "\"]")).text(numObj[addedItemType]);
-    numObj.合計 += parseInt(itemLengthArray[latestNum() - 1].金額);
-    $totalNum.text(numObj.合計);
-  };
-
-  var resetInput = function resetInput() {
-    $inputArea.find('input').val('');
-    $inputArea.find('select').val('選択してください');
-  };
-
-  var funcAddItem = function funcAddItem() {
-    if ($inputArea.find('.p-itemNum').val() === '' || $inputArea.find('select').val() === '選択してください') {
-      showAttention();
-    } else {
-      hiddenAttention();
-      setTempObj();
-      addListNum();
-      addLocalStorage();
-      addTable();
-      update();
-      resetInput();
-    }
-  };
+  }
 });
+
+var updateListCount = function updateListCount() {
+  app.listCount = localStorage.getItem('latestCount');
+};
+
+var updateLists = function updateLists() {
+  for (var i = 1; i <= app.listCount; i++) {
+    app.lists.push(JSON.parse(localStorage.getItem(i)));
+  }
+};
+
+var reCulculation = function reCulculation() {
+  var listsLength = app.lists.length;
+
+  var totalCulculation = function totalCulculation() {
+    for (var i = 0; i < listsLength; i++) {
+      app.prices.total += parseInt(app.lists[i].price);
+    }
+  };
+
+  var foodsCulculation = function foodsCulculation() {
+    for (var i = 0; i < listsLength; i++) {
+      if (app.lists[i].type === 'foods') {
+        app.prices.foods += parseInt(app.lists[i].price);
+      }
+    }
+  };
+
+  var commodityCulculation = function commodityCulculation() {
+    for (var i = 0; i < listsLength; i++) {
+      if (app.lists[i].type === 'commodity') {
+        app.prices.commodity += parseInt(app.lists[i].price);
+      }
+    }
+  };
+
+  var publicCulculation = function publicCulculation() {
+    for (var i = 0; i < listsLength; i++) {
+      if (app.lists[i].type === 'public') {
+        app.prices["public"] += parseInt(app.lists[i].price);
+      }
+    }
+  };
+
+  totalCulculation();
+  foodsCulculation();
+  commodityCulculation();
+  publicCulculation();
+};
+
+updateListCount();
+updateLists();
+reCulculation();
