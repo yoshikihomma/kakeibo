4afa5b90d3a4f2db4f2f2745e46e50b6580178f7
本間睦貴 <yoshikihomma@gmail.com>
2020-07-05 02:18:26 +0900

    メイン画面のマークアップ完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 const $PRICE = document.getElementsByClassName('p-price');
+const $INPUT_WHO = document.getElementsByClassName('p-itemwho');
 const $INPUT_TYPE = document.getElementsByClassName('p-itemtype');
 const $INPUT_PRICE = document.getElementsByClassName('p-itemprice');
 const LIST_REF = firebase.firestore().collection("users").doc('list');
@@ -9,16 +10,48 @@ let listNum = 0;
 const app = new Vue({
   el: "#app",
   data: {
-    prices: {
-      total: 0,
-      foods: 0,
-      commodity: 0,
-      public: 0,
+    a: {
+      prices: {
+        total: 0,
+        foods: {
+          result: 0,
+          sum: 0,
+        },
+        commodity: {
+          result: 0,
+          sum: 0,
+        },
+        public: {
+          result: 0,
+          sum: 0,
+        },
+      },
     },
+    b: {
+      prices: {
+        total: 0,
+        foods: {
+          result: 0,
+          sum: 0,
+        },
+        commodity: {
+          result: 0,
+          sum: 0,
+        },
+        public: {
+          result: 0,
+          sum: 0,
+        },
+      },
+    },
+    take: '',
+    give: '',
+    deffer: 0,
     showModal: true,
     showAttention: false,
-    keyArray: ['date', 'name', 'type', 'price'],
+    keyArray: ['who', 'date', 'name', 'type', 'price'],
     tempObj: {
+      'who': '選択してください',
       'date': '', 
       'name': '', 
       'type': '選択してください', 
@@ -31,7 +64,8 @@ const app = new Vue({
       const tempUpdate = () => {
         for (let i = 0; i < app.keyArray.length; i ++) {
           app.tempObj[app.keyArray[i]] = document.getElementsByClassName(`p-item${app.keyArray[i]}`)[0].value;
-        } 
+        }
+        console.log(app.tempObj['who']);
       };
       
       const addDB = () => {
@@ -47,7 +81,7 @@ const app = new Vue({
           console.error("Error writing document: ", error);
         });
       };
-      
+
       const resetInput = () => {
         document.itemInput.reset();
       };
@@ -60,7 +94,7 @@ const app = new Vue({
         app.showAttention = false;
       };
       
-      if ($INPUT_TYPE[0].value === '選択してください' || $INPUT_PRICE[0].value  === "") {
+      if ($INPUT_WHO[0].value === '選択してください' || $INPUT_TYPE[0].value === '選択してください' || $INPUT_PRICE[0].value  === "") {
         showAttention()
       } else {
         tempUpdate();
@@ -74,47 +108,85 @@ const app = new Vue({
 
 LIST_REF.onSnapshot((doc) => {
   const list = doc.data().listArray;
-  let foodsSum = 0;
-  let commoditySum = 0;
-  let publicSum = 0;
 
   listNum = list.length;
 
   const foodsCulculater = () => {
     for(let i = 0; i < listNum; i ++) {
-      if(list[i].type === 'foods') {
-        foodsSum += parseInt(list[i].price);
-        app.prices.foods = foodsSum;
+      if (list[i].who === 'a') {
+        if(list[i].type === 'foods') {
+          app.a.prices.foods.sum += parseInt(list[i].price);
+          app.a.prices.foods.result = app.a.prices.foods.sum;
+        }
+      } else {
+        if(list[i].type === 'foods') {
+          app.b.prices.foods.sum += parseInt(list[i].price);
+          app.b.prices.foods.result = app.b.prices.foods.sum;
+        }
       }
     }
+    app.a.prices.foods.sum = 0;
+    app.b.prices.foods.sum = 0;
   };
 
   const commodityCulculater = () => {
     for(let i = 0; i < listNum; i ++) {
-      if(list[i].type === 'commodity') {
-        commoditySum += parseInt(list[i].price);
-        app.prices.commodity = commoditySum;
+      if (list[i].who === 'a') {
+        if(list[i].type === 'commodity') {
+          app.a.prices.commodity.sum += parseInt(list[i].price);
+          app.a.prices.commodity.result = app.a.prices.commodity.sum;
+        }
+      } else {
+        if(list[i].type === 'commodity') {
+          app.b.prices.commodity.sum += parseInt(list[i].price);
+          app.b.prices.commodity.result = app.b.prices.commodity.sum;
+        }
       }
     }
+    app.a.prices.commodity.sum = 0;
+    app.b.prices.commodity.sum = 0;
   };
 
   const publicCulculater = () => {
     for(let i = 0; i < listNum; i ++) {
-      if(list[i].type === 'public') {
-        publicSum += parseInt(list[i].price);
-        app.prices.public = publicSum;
+      if (list[i].who === 'a') {
+        if(list[i].type === 'public') {
+          app.a.prices.public.sum += parseInt(list[i].price);
+          app.a.prices.public.result = app.a.prices.public.sum;
+        }
+      } else {
+        if(list[i].type === 'public') {
+          app.b.prices.public.sum += parseInt(list[i].price);
+          app.b.prices.public.result = app.b.prices.public.sum;
+        }
       }
+    }
+    app.a.prices.public.sum = 0;
+    app.b.prices.public.sum = 0;
+  };
+
+  const totalCulculater = () => {
+    for(let i = 0; i < listNum; i ++) {
+      app.a.prices.total = app.a.prices.foods.result + app.a.prices.commodity.result + app.a.prices.public.result;
+      app.b.prices.total = app.b.prices.foods.result + app.b.prices.commodity.result + app.b.prices.public.result;
     }
   };
 
-  const totalCulculation = () => {
-    for(let i = 0; i < listNum; i ++) {
-      app.prices.total = foodsSum + commoditySum + publicSum;
+  const defferCulculater = () => {
+    const deffer = app.a.prices.total - app.b.prices.total;
+    if (deffer >= 0) {
+      app.take = 'B';
+      app.give = 'A';
+    } else {
+      app.take = 'A';
+      app.give = 'B';
     }
-  };
+    app.deffer = parseInt(deffer) / 2;
+  }
 
   foodsCulculater();
   commodityCulculater();
   publicCulculater();
-  totalCulculation();
+  totalCulculater();
+  defferCulculater()
 });
